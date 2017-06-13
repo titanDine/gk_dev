8fe7983aca7b22a56177c58538bf1a07855c809b
mailer <mail@mail.com>
2017-06-13 17:36:48 +0100

    adding NUnit tests - removing built in ms mvc tests

---
+++ src/gk_teamcity_test.UnitTests/MockTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace gk_teamcity_test.UnitTests
+{
+    [TestFixture]
+    public class MockTest
+    {
+        [Test]
+        public void CheckSum()
+        {
+            var testValue = 2;
+            Assert.AreEqual(testValue, 2);
+        }
+
+        [Test]
+        public void CheckSum2()
+        {
+            var testValue = 2;
+            Assert.AreNotEqual(testValue, 3);
+        }
+    }
+}
